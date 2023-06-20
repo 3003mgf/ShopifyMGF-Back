21c76e0f81a18655f457c21626141553cb7a0f1a
Ignacio Gramajo Feijoo <118638937+3003mgf@users.noreply.github.com>
2023-06-20 15:26:59 -0300

    Mongo Atlas Commit

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 
 const dbConnect = async() =>{
   try{
-    const connection = await mongoose.connect("mongodb://localhost:27017/eCommerce");
+    const connection = await mongoose.connect("mongodb+srv://gramajofeijoonacho:aEVtW0IquetzFfBl@shopifycluster.pji2avj.mongodb.net/?retryWrites=true&w=majority");
 
     console.log("Database connected succesfully!");
   }catch(err){
