a345c5d5f5dedd856741db4971dc304689380f4e
Ignacio Gramajo Feijoo <118638937+3003mgf@users.noreply.github.com>
2023-06-20 20:35:53 -0300

    abc

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = async() =>{
   try{
-    await mongoose.connect("mongodb://mongo:grMeLHI7qvV3A8x7OOTy@containers-us-west-89.railway.app:7120");
+    await mongoose.connect("mongodb://mongo:Nc92EBkgJvATMlAN2UFn@containers-us-west-207.railway.app:6410");
 
     console.log("Database connected succesfully!");
   }catch(err){
