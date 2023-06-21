ba396c8ee2effda79bc5653a27a610c520037a8a
Ignacio Gramajo Feijoo <118638937+3003mgf@users.noreply.github.com>
2023-06-21 00:23:21 -0300

    0

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app"),
       port = app.get("port");
 
-app.listen(port, "localhost", ()=>{
+app.listen(port, ()=>{
   console.log("Server running at localhost:%d", port);
 })
